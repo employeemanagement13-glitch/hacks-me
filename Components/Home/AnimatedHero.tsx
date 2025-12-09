000341046ddd983d14bdb4d398da7b58151ffabb
employeemanagement13-glitch <employeemanagement13@gmail.com>
2025-12-10 00:29:39 +0500

    Update AnimatedHero.tsx

--- Components/Home/AnimatedHero.tsx
+++ Components/Home/AnimatedHero.tsx
@@ -1,14 +1,1 @@
-// Components/Home/AnimatedHero.tsx
-"use client";
 
-import React from "react";
-import HeroContainer from "@/Components/hero/HeroContainer";
-
-export default function AnimatedHero() {
-  // HeroContainer now handles all scroll logic itself
-  return (
-    <main className="pt-10 bg-[#0A0A0A]">
-      <HeroContainer />
-    </main>
-  );
-}
